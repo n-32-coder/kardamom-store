cf27d0ee966995f2b13a6614ff4408b9bde5b7a8
Nandana Shaji <nandanashaji32@gmail.com>
2026-09-09 20:13:26 +0530

    Cardamom Powder: real product photo

---
+++ verify_powder_photo.py
@@ -0,0 +1,25 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.test import Client
+
+from products.models import Product
+
+p = Product.objects.get(category__slug='powder')
+p.image = 'products/powder.png'
+p.save(update_fields=['image'])
+print(f'image -> {p.image} on {p.name}')
+
+c = Client()
+r = c.get('/products/')
+html = r.content.decode()
+assert r.status_code == 200 and 'powder.png' in html
+print('listing shows photo: OK')
+r = c.get(p.get_absolute_url())
+assert r.status_code == 200 and 'powder' in r.content.decode()
+print('detail shows photo: OK')
+print('POWDER PHOTO OK')
